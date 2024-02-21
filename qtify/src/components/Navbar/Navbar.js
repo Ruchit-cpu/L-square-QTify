989e688e60de91a13eba7b7b89add7b4b0a29ef2
Ruchit <ruchit2502@gmail.com>
2024-02-22 01:11:32 +0530

    navbar

---
+++ qtify/src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import Logo from "../Logo/Logo";
+import Searchbar from "../SearchBar/Searchbar";
+import Button from "../Button/Button";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.Navbar}>
+      <Logo />
+      <Searchbar />
+      <Button />
+    </nav>
+  );
+}
---
+++ qtify/src/components/SearchBar/Searchbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+import SearchIcon from "../../assets/search-icon.svg";
+
+export default function Searchbar() {
+  return (
+    <div className={styles.container}>
+      <input
+        placeholder="Search a album of your choice"
+        className={styles.search}
+      />
+      <button className={styles.searchButton}>
+        <img src={SearchIcon} alt="searchIcon" />
+      </button>
+    </div>
+  );
+}
