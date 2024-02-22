d337f72a2bff240db38681a828810b78c9df2078
Ruchit <ruchit2502@gmail.com>
2024-02-22 11:02:53 +0530

    navbar final 3

--- qtify/src/components/Logo/Logo.js
+++ qtify/src/components/Logo/Logo.js
@@ -5,12 +5,7 @@ import style from "./Logo.module.css";
 export default function Logo() {
   return (
     <div className={style.logo}>
-      <img
-        src={LogoImage}
-        alt="Qtify Logo"
-        width={67}
-        style={{ padding: "20px 0" }}
-      />
+      <img src={LogoImage} alt="Qtify Logo" width={67} />
     </div>
   );
 }
